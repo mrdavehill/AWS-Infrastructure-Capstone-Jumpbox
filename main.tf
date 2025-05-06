213213f71c399845757e38feba2e9b81fe994205
Dave Hill <dave@davehill.org>
2025-05-06 10:51:05 +0100

    security group

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_security_group_rule" "ingress" {
   type              = "ingress"
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = []"0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 22
   security_group_id = aws_security_group.this.id
 }
